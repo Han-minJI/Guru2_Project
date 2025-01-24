9c88a6b4d24a980d24ec17d6ec38e478920f3ff3
이석현 <jrkenniya@gmail.com>
2025-01-24 14:54:08 +0900

    - 복약 체크 Table 관련 코드 추가

--- app/src/main/java/com/example/guru2_project/DBManager.kt
+++ app/src/main/java/com/example/guru2_project/DBManager.kt
@@ -25,11 +25,24 @@ class DBManager(context: Context?,
                     "userId TEXT PRIMARY KEY, " +
                     "userPasswd TEXT)"
         )
+
+        // 복약 체크 테이블 --> "복약 등록하기" 버튼 클릭 시 생성
+        db?.execSQL(
+            "CREATE TABLE IF NOT EXISTS mediTBL (" +
+                "medi_num INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "user_id TEXT, " +
+                "medi_name TEXT, " +
+                "medi_day_of_week TEXT," +
+                "medi_time TEXT," +
+                "medi_date TEXT," +
+                "medi_check INTEGER)"
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db?.execSQL("DROP TABLE IF EXISTS member")
         db?.execSQL("DROP TABLE IF EXISTS session")
+        db?.execSQL("DROP TABLE IF EXISTS mediTBL") // 복약 체크 테이블
         onCreate(db)
     }
 }
