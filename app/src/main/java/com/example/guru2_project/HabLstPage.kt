0827d4f23be136cd42f9e55cd984164d3eab0080
이석현 <jrkenniya@gmail.com>
2025-01-26 22:37:09 +0900

    - 개발중 화면 관련 파일 추가(kt, layout, img 파일) - 개발중 화면에서 닫기 아이콘/확인 버튼 클릭 시 이전 화면으로 이동하는 기능 추가 - 생활 습관 체크 관련 파일에 주석 추가

--- app/src/main/java/com/example/guru2_project/HabLstPage.kt
+++ app/src/main/java/com/example/guru2_project/HabLstPage.kt
@@ -23,33 +23,31 @@ class HabLstPage : AppCompatActivity() {
     lateinit var btnBckToMain : ImageButton // 메인 화면 이동 Button
     lateinit var btnInitHab : Button // 체크리스트 전체 삭제 Button
 
-    // "복용하시는 약을 등록해주세요" -> MedRegPage(복약 등록) 페이지로 이동 위한 Button
+    // "오늘의 목표 리스트를 등록해주세요" -> HabRegPage(나의 생활 체크 등록) 페이지로 이동 위한 Button
     lateinit var btnToHabReg : ImageButton
 
-    lateinit var habLstLayout : LinearLayout // 복약 리스트(CheckBox)가 나열될 LinearLayout
+    lateinit var habLstLayout : LinearLayout // 생활 습관 리스트(CheckBox)가 나열될 LinearLayout
 
-    // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
     lateinit var dbManager : DBManager    // DBManger 객체
     lateinit var sqlitedb : SQLiteDatabase   // SQLiteDatabase 객체
-    // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
 
 
-    // medNameLst, medTimeLst -> 복약 리스트에 나열되는 CheckBox의 내용을 "복용할 약, 복용 예정 시간"으로 세팅해주기 위해 필요
-    lateinit var habNameLst : MutableList<String>       // mediTBL(복약 테이블)의 medi_name 필드의 값만 저장한 가변 리스트
-    lateinit var habTimeLst : MutableList<String>       // mediTBL(복약 테이블)의 medi_time 필드의 값만 저장한 가변 리스트
-    lateinit var habCntLst : MutableList<String>
-    lateinit var habDayOfWeekLst : MutableList<String>  // mediTBL(복약 테이블)의 medi_day_of_week 필드의 값만 저장한 가변 리스트
-    lateinit var habCheckBoxLst : MutableList<CheckBox> // 화면에 표시될 복약 CheckBox를 담을 가변 리스트
+    // habNameLst, habTimeLst, habCntLst -> 생활 습관 리스트에 나열되는 CheckBox의 내용에 "습관 이름, 습관 시간대, 습관 자세한 내용"으로 세팅해주기 위해 필요
+    lateinit var habNameLst : MutableList<String>       // habitTBL(생활 습관 테이블)의 habit_name 필드의 값만 저장한 가변 리스트
+    lateinit var habTimeLst : MutableList<String>       // habitTBL(생활 습관 테이블)의 habit_time 필드의 값만 저장한 가변 리스트
+    lateinit var habCntLst : MutableList<String>        // habitTBL(생활 습관 테이블)의 habit_content 필드의 값만 저장한 가변 리스트
+    lateinit var habDayOfWeekLst : MutableList<String>  // habitTBL(생활 습관 테이블)의 habit_day_of_week 필드의 값만 저장한 가변 리스트
+    lateinit var habCheckBoxLst : MutableList<CheckBox> // 화면에 표시될 생활 습관 CheckBox를 담을 가변 리스트
 
-    var habitNum: Int = 0                // mediTBL의 medi_num (PRIMARY KEY) 필드의 값을 가져올 변수
-    lateinit var habName: String        // medNameLst에 원소를 추가하기 위해 필요한 변수
-    lateinit var habTime: String        // medTimeLst에 원소를 추가하기 위해 필요한 변수
-    lateinit var habCnt: String
-    lateinit var habDayOfWeek: String   // medDayOfWeekLst에 원소를 추가하기 위해 필요한 변수
+    var habitNum: Int = 0                // habitTBL habit_num (PRIMARY KEY) 필드의 값을 가져올 변수
+    lateinit var habName: String        // habNameLst에 원소를 추가하기 위해 필요한 변수
+    lateinit var habTime: String        // habTimeLst에 원소를 추가하기 위해 필요한 변수
+    lateinit var habCnt: String         // habCntLst에 원소를 추가하기 위해 필요한 변수
+    lateinit var habDayOfWeek: String   // habDayOfWeekLst에 원소를 추가하기 위해 필요한 변수
     lateinit var habDate: String        // 사용자가 각각의 체크박스를 마지막으로 클릭한 시점을 저장할 변수
-    var habCheck: Int? = null
+    var habCheck: Int? = null           // habitTBL habit_check 필드의 값을 가져올 변수
 
-    lateinit var todayDayOfWeek: String // 실행한 시간의 요일 -> 복약 리스트에 오늘 요일에 해당하는 CheckBox만 표시해주기 위해 필요한 변수
+    lateinit var todayDayOfWeek: String // 실행한 시간의 요일 -> 습관 리스트에 오늘 요일에 해당하는 CheckBox만 표시해주기 위해 필요한 변수
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,14 +57,14 @@ class HabLstPage : AppCompatActivity() {
         btnBckToMain = findViewById(R.id.btnBckToMain)
         btnInitHab = findViewById(R.id.btnInitHab)
 
-        btnToHabReg = findViewById(R.id.btnToHabReg)    // MedRegPage (복용약 등록 화면) 으로 이동하는 버튼 -> 복용하시는 약을 등록해주세요
-        habLstLayout = findViewById(R.id.habLstLayout)  // 복용 체크리스트가 표시될 레이아웃
+        btnToHabReg = findViewById(R.id.btnToHabReg)    // HabRegPage (생활 습관 등록 화면) 으로 이동하는 버튼 -> "오늘의 목표 리스트를 등록해주세요"
+        habLstLayout = findViewById(R.id.habLstLayout)  // 목표 체크리스트가 표시될 레이아웃
 
         // 화면에 표시되어있던 체크리스트를 우선 없애고 시작
         onResume()
 
-        btnToHabReg.setOnClickListener { // "복용하시는 약을 등록해주세요" 버튼 클릭 시
-            // MedRegPage(복약 등록 화면)으로 Intent 전달하며 화면 전환
+        btnToHabReg.setOnClickListener { // "오늘의 목표 리스트를 등록해주세요" 버튼 클릭 시
+            // HabRegPage(생활 습관 등록 화면)으로 Intent 전달하며 화면 전환
             var intent = Intent(this, HabRegPage::class.java)
             startActivity(intent)
         }
@@ -80,21 +78,19 @@ class HabLstPage : AppCompatActivity() {
         btnInitHab.setOnClickListener { // 전체 삭제 버튼 클릭 시
             var nowUserID = getCurrentUserId() // 함수 호출하여 현재 로그인한 사용자 ID 가져오기
 
-            // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
             // 값을 읽어오기만 할 것이므로 읽기 전용으로 DB 열기
             dbManager = DBManager(this, "userDB", null, 11)
             sqlitedb = dbManager.readableDatabase
 
-            // mediTBL에서 레코드 삭제 (user_id 필드의 값 == 현재 로그인한 사용자의 ID인 경우에만)
+            // habitTBL에서 레코드 삭제 (user_id 필드의 값 == 현재 로그인한 사용자의 ID인 경우에만)
             sqlitedb.execSQL("DELETE FROM habitTBL WHERE user_id = '" + nowUserID + "';")
             habLstLayout.removeAllViews() // 레이아웃에서도 모든 View 객체 지우기
 
-            // 복약 체크리스트가 삭제되었다는 토스트 메시지 출력
-            Toast.makeText(this@HabLstPage, "복약 체크리스트 전체 삭제됨", Toast.LENGTH_SHORT).show()
+            // 생활 습관(목표) 체크리스트가 삭제되었다는 토스트 메시지 출력
+            Toast.makeText(this@HabLstPage, "목표 체크리스트 전체 삭제됨", Toast.LENGTH_SHORT).show()
 
             sqlitedb.close()
             dbManager.close()
-            // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
         }
     }
     // 오늘 무슨 요일인지 가져오는 함수
@@ -103,7 +99,7 @@ class HabLstPage : AppCompatActivity() {
         var todayDayOfWeek: String? = null              // 오늘 요일 (앱 실행일 기준)을 저장할 변수 초기화
         val nWeek: Int = cal.get(Calendar.DAY_OF_WEEK)  // 요일 값 (Int 타입) 가져오기
 
-        if (nWeek == 1) {      // 요일 값 = 1 -> 일요일
+        if (nWeek == 1) {        // 요일 값 = 1 -> 일요일
             todayDayOfWeek = "sun"
         } else if (nWeek == 2) { // 요일 값 = 2 -> 월요일
             todayDayOfWeek = "mon"
@@ -118,34 +114,33 @@ class HabLstPage : AppCompatActivity() {
         } else if (nWeek == 7) { // 요일 값 = 7 -> 토요일
             todayDayOfWeek = "sat"
         }
-        return todayDayOfWeek // 오늘 무슨 요일인지 반환
+        return todayDayOfWeek   // 오늘 무슨 요일인지 반환
     }
 
     // 다시 앱으로 돌아왔을 때 호출되는 함수 재정의
     override fun onResume() {
         super.onResume()
 
-        // <화면 새로고침한 뒤, 복약 CheckBox 동적으로 추가> 함수 호출
+        // <화면 새로고침한 뒤, 나의 생활 CheckBox 동적으로 추가>하는 함수 호출
         refreshMedList()
     }
 
-    // 복약 체크리스트 하나씩 추가하는 함수
+    // 나의 생활 체크리스트 하나씩 추가하는 함수
     private fun refreshMedList() {
         habLstLayout.removeAllViews() // 우선, 해당 레이아웃에서 모든 뷰 삭제 후 진행
 
         var nowUserID = getCurrentUserId() // 함수 호출하여 현재 로그인한 사용자 ID 가져오기
 
-        // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
         // 값을 Update해야 하므로 쓰기 전용으로 DB 열기
         dbManager = DBManager(this, "userDB", null, 11)
         sqlitedb = dbManager.writableDatabase
 
         todayDayOfWeek = getDayOfWeek().toString() // 오늘 요일 알아내는 함수 호출
 
-        // mediTBL(복약 등록 테이블)의 값을 업데이트 (현재 로그인한 사용자와 관련된 레코드만)
-        // <하루가 지난 상태>인 경우 -> [medi_check] 필드 값을 0으로, [medi_date] 필드 값을 오늘 날짜로 업데이트
+        // habitTBL(나의 생활 습관 등록 테이블)의 값을 업데이트 (현재 로그인한 사용자와 관련된 레코드만)
+        // <하루가 지난 상태>인 경우 -> [habit_check] 필드 값을 0으로, [habit_date] 필드 값을 오늘 날짜로 업데이트
         // 하루가 지나면 체크된 체크박스도 전부 초기화해줘야 함. 그럴려면 마지막으로 앱을 실행한 날짜를 알아야함
-        // 따라서 medi_date 필드에 지금 현재 앱을 실행한 날짜를 주기적으로 기록해주는 것.
+        // 따라서 habit_date 필드에 지금 현재 앱을 실행한 날짜를 주기적으로 기록해주는 것.
         sqlitedb.execSQL("UPDATE habitTBL SET habit_check = 0 WHERE habit_date != '${LocalDate.now()}' AND user_id = '$nowUserID';")
         sqlitedb.execSQL("UPDATE habitTBL SET habit_date = '${LocalDate.now()}' WHERE habit_date != '${LocalDate.now()}' AND user_id = '$nowUserID';")
 
@@ -155,7 +150,7 @@ class HabLstPage : AppCompatActivity() {
             null
         )
 
-        // mediTBL(테이블)의 각각의 필드의 값을 저장할 가변 리스트 초기화
+        // habitTBL(테이블)의 각각의 필드의 값을 저장할 가변 리스트 초기화
         habNameLst = mutableListOf()
         habTimeLst = mutableListOf()
         habCntLst = mutableListOf()
@@ -175,15 +170,18 @@ class HabLstPage : AppCompatActivity() {
             // CheckBox 객체를 동적으로 추가하여 속성 적용
             val checkBox = CheckBox(this).apply {
                 // checkBox의 text 값에 들어갈 spannable
-                val spannable = SpannableString("$habName $habCnt\n$habTime") // habName은 생활 습관 이름, habTime은 생활 습관 예정 시간
-                spannable.setSpan(StyleSpan(Typeface.BOLD), 0, habName.length, 0) // 생활 습관 이름은 Bold체로 표시
-                spannable.setSpan(AbsoluteSizeSpan(80), 0, habName.length, 0) // 생활 습관 이름은 80px로 지정
+                val spannable = SpannableString("$habName $habCnt\n$habTime") // habName은 생활 습관 이름, habCnt는 생활 습관의 자세한 내용, habTime은 생활 습관 예정 시간
 
+                // 나의 생활 습관 이름 스타일 지정 - Bold체, 크기는 80px
+                spannable.setSpan(StyleSpan(Typeface.BOLD), 0, habName.length, 0)
+                spannable.setSpan(AbsoluteSizeSpan(80), 0, habName.length, 0)
+
+                // 나의 생활 습관 자세한 내용 스타일 지정 - ITALIC체, 크기는 40px, padding 속성 적용
                 spannable.setSpan(StyleSpan(Typeface.ITALIC), habName.length + 1, habName.length + 1 + habCnt.length, 0)
                 spannable.setSpan(AbsoluteSizeSpan(40), habName.length + 1, habName.length + 1 + habCnt.length, 0)
                 spannable.setSpan(setPadding(20,0,0,0), habName.length + 1, habName.length + 1 + habCnt.length, 0)
 
-                // 생활 습관 예정 시간의 text color와 padding 설정
+                // 나의 생활 습관 예정 시간 text color와 padding 설정
                 spannable.setSpan(ForegroundColorSpan(Color.parseColor("#272D4F")), habName.length + 1 + habCnt.length + 1, spannable.length, 0)
                 spannable.setSpan(setPadding(20,0,0,0), habName.length + 1 + habCnt.length + 1, spannable.length, 0)
 
@@ -198,34 +196,32 @@ class HabLstPage : AppCompatActivity() {
 
                 // 체크 상태가 변경된 경우(즉, 사용자가 체크박스를 클릭한 경우) 이벤트 처리
                 setOnCheckedChangeListener { _, isChecked ->
-                    updateMediCheck(habitNum, isChecked) // DB의 mediTBL에 check 여부를 저장하는 함수 호출
+                    updateMediCheck(habitNum, isChecked) // DB의 habitTBL의 check 여부를 저장하는 함수 호출
                 }
                 val params = LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT) // checkBox를 담을 레이아웃 높이와 너비 지정
-                // 아래쪽 여백 (체크 박스 간의 위아래 간격 조정을 위해 위쪽 여백&아래쪽 여백 설정
+                // 체크 박스 간의 위아래 간격 조정을 위해 위쪽 여백&아래쪽 여백 설정
                 params.topMargin = 20
                 params.bottomMargin = 20
                 layoutParams = params
             }
-            habCheckBoxLst.add(checkBox) // 복약 체크리스트에 해당 체크박스 추가
+            habCheckBoxLst.add(checkBox) // 나의 생활 체크리스트에 해당 체크박스 추가
             habLstLayout.addView(checkBox) // 화면에 해당 체크박스 추가하여 보여줌
         }
         cursor.close()
         sqlitedb.close()
         dbManager.close()
-        // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
     }
 
-    // DB의 mediTBL에 check 여부를 저장하는 함수
+    // DB의 habitTBL에 check 여부를 저장하는 함수
     private fun updateMediCheck(habitNum: Int, isChecked: Boolean) {
         var nowUserID = getCurrentUserId() // 현재 로그인한 사용자 ID 가져오기
 
-        // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
         // 값을 업데이트해야 하므로 읽기 전용으로 DB 열기
         dbManager = DBManager(this, "userDB", null, 11)
         val sqlitedb = dbManager.writableDatabase
 
         // 이미 닫힌 DB를 reopen 하는 에러를 방지하기 위해 try-catch-finally로 작성
-        try { //
+        try { // CheckBox의 체크 상태에 따라 habitTBL의 체크 여부 필드(habit_check)에 값을 저장
             sqlitedb.execSQL(
                 "UPDATE habitTBL SET habit_check = ${if (isChecked) 1 else 0} WHERE habit_num = '$habitNum' AND user_id = '$nowUserID';"
             )
@@ -235,14 +231,12 @@ class HabLstPage : AppCompatActivity() {
             sqlitedb.close()
             dbManager.close()
         }
-        // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
     }
 
     // 현재 로그인한 사용자 ID 가져오는 함수
     private fun getCurrentUserId(): String? {
         var userId: String? = null // 로그인 사용자 ID를 저장할 변수
 
-        // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
         // 값을 가져오기만 할 것이므로 읽기 전용으로 DB 열기
         dbManager = DBManager(this, "userDB", null, 11)
         val sqlitedb = dbManager.readableDatabase
@@ -253,7 +247,6 @@ class HabLstPage : AppCompatActivity() {
         }
         cursorId.close()
         sqlitedb.close()
-        // !!! DBManager에 코드 추가 -> 버전 업글 -> 주석 해제 !!!
 
         return userId // 가져온 로그인 사용자 ID 리턴
     }
